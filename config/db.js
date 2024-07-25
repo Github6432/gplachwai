c6ffe964ffaa30e4c6b57e8dca703f30916335cc
Github6432 <parasshankhwar2@gmail.com>
2024-07-25 16:11:58 +0530

    server and database config successfuly

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ require('colors');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Connected to Mongodb Database ${conn.connection.host}`);
+        console.log(`Connected to Mongodb Database ${conn.connection.host}`.bgMagenta);
     } catch (error) {
         console.log(`Error in MongoDB ${error}`.bgRed.white)
     }
 }
 
-export default connectDB();
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 require('colors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
+
 //rest object
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get('/', (req, res) => {
@@ -14,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9000;
 
 
 //RUN LISTEN
